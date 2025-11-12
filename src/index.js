f1da58067c250d8c6aacea9e1d710643650ebf88
radha singla <singlaradha19@gmail.com>
2025-11-12 18:05:42 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,39 +4,62 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { connectDb } = require("./db/config");
+const cluster = require("cluster");
+const os = require("os");
 const Message = require("./constant/message").en;
 
-// Initialize Express app
-const app = express();
+if (cluster.isPrimary) {
+  console.log(`master ${process.pid} is running`);
 
-//Middleware code
-app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    method: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-app.use(morgan("dev"));
-app.use(require("./middleware/res"));
-app.use("/api", require("./router"));
+  const numCpus = os.cpus().length;
+  console.log(numCpus);
+  
 
-app.get("/data", (req, res) => {
-  res.json({ message: "CORS is enabled for all!" });
-});
 
-app.get("/", (req, res) => {
-  res.send("Project is live on Render!");
-});
-app.use((error, req, res, next) => {
-  console.log(error);
-  return res
-    .status(error.status || 400)
-    .json({ message: error.message || Message.INTERNAL_SERVER_ERROR });
-});
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+  }
+  cluster.on("online", (worker) => {
+    console.log(`worker ${worker.process.pid} is online`);
+  });
+  cluster.on("exit", (worker) => {
+    console.log(`worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  // Initialize Express app
+  const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`server connected successfully ${process.env.PORT}`);
-  connectDb();
-});
+  //Middleware code
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(require("./middleware/res"));
+  app.use("/api", require("./router"));
+
+  app.use(
+    cors({
+      origin: "*",
+      method: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+
+  app.get("/data", (req, res) => {
+    res.json({ message: "CORS is enabled for all!" });
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Project is live on Render!");
+  });
+  app.use((error, req, res, next) => {
+    console.log(error);
+    return res
+      .status(error.status || 400)
+      .json({ message: error.message || Message.INTERNAL_SERVER_ERROR });
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`server connected successfully ${process.env.PORT}`);
+    connectDb();
+  });
+}
--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -2,6 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
